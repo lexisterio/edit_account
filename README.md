6afdd7c14d6b0a7886f7fbea41a0c67b8d482e74
lexisterio <31936747+lexisterio@users.noreply.github.com>
2018-03-01 00:45:29 -0500

    commit final finishes

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# edit_account
+## User_Research_Login
+- By: Lexi Sterio
+
+## Prerequisites
+- Atom
+- Terminal
+- PHPmyAdmin
+
+## Websites used for reference
+- http://php.net/manual/en/reserved.variables.session.php
+- http://php.net/manual/en/function.date.php
+- http://php.net/manual/en/language.operators.comparison.php
+
+## Step 1
+- Allow the user to login and edit the account (done in class)
+
+## Step 2
+- When the new user logs in for the first time they are sent to edit their account, skipping the admin page
+- in order to do this you create a new field in the database user_first, by default this value
+is set to 1
+- After the first login of the user, we change this value to 0
+
+# Step 3
+- New user has a time limit to login after their account has been created. If they do not login before the time expires their account will be suspended.
+- For this to happen you need to use the date/time function from php in order to create two date objects and make a comparison between them
+- you will get the time difference between the too objects in minutes
+- you will set the max time value of the users first login and then you will use this value to compare it with the difference in time from when they first logged in
+
+## Authors
+- Lexi Sterio
--- admin/phpscripts/login.php
+++ admin/phpscripts/login.php
@@ -2,6 +2,9 @@
 
 	function logIn($username, $password, $ip) {
 		require_once('connect.php');
+
+		$maxTime = 30;//Minutes
+
 		$username = mysqli_real_escape_string($link, $username);
 		$password = mysqli_real_escape_string($link, $password);
 		$loginstring = "SELECT * FROM tbl_user WHERE user_name='{$username}' AND user_pass='{$password}'";
@@ -9,14 +12,43 @@
 		//echo mysqli_num_rows($user_set);
 		if(mysqli_num_rows($user_set)){
 			$founduser = mysqli_fetch_array($user_set, MYSQLI_ASSOC);
-			$id = $founduser['user_id'];
-			$_SESSION['user_id'] = $id;
-			$_SESSION['user_name']= $founduser['user_fname'];
-			if(mysqli_query($link, $loginstring)){
-				$update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
-				$updatequery = mysqli_query($link, $update);
+
+			//Verify if is first login of the user and time is valid
+			$firstLogin = $founduser['user_first'];
+			$dateCreate = new Date($founduser['user_date']);
+			//Compare from now
+			$dateNow = new Date();
+			//Difference in minutes
+			$interval = $dateCreate->diff($dateNow);
+			$minutesDiff = $interval->format('%i');//%i minutes format
+
+			if($minutesDiff>$maxTime && $firstLogin == 1){
+				$message = "Too much time your account is blocked";
+				return $message;
+			}else{
+				$id = $founduser['user_id'];
+				$_SESSION['user_id'] = $id;
+				$_SESSION['user_name']= $founduser['user_fname'];
+
+				if(mysqli_query($link, $loginstring)){
+					$update = "UPDATE tbl_user SET user_ip='{$ip}' WHERE user_id={$id}";
+					$updatequery = mysqli_query($link, $update);
+				}
+
+				//Verify if this is the first login of the user, if it is, you will send the user to the edit_account
+
+				if($firstLogin == 1){
+					//we need to update the first time field to 0
+					$update = "UPDATE tbl_user SET user_first=0 WHERE user_id={$id}";
+					redirect_to("admin_edituser.php");
+				}else{
+					//if this is not the first login, it will redirct the user to the admin section
+					redirect_to("admin_index.php");
+				}
+
 			}
-			redirect_to("admin_index.php");
+
+
 		}else{
 			$message = "Learn how to type you dumba&*.";
 			return $message;
